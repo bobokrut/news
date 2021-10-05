89e40bb2f3353ced8e869d6bd401d1dbb37766af
bobokrut <egor-evlampiev@mail.ru>
2021-10-05 21:21:41 +0300

    fix: now article id is updated from website on init

--- news/dtp_ptz.py
+++ news/dtp_ptz.py
@@ -1,5 +1,7 @@
 from urllib.parse import urljoin
 
+from loguru import logger
+
 from .news import News
 
 
@@ -9,7 +11,15 @@ class DtpPtz(News):
         super().__init__(+3)
 
         self._URLS = ('https://dtpptz.ru/', )
-        self._last_id = 12635
+        self._last_id = self._get_last_news_id()
+
+    def _get_last_news_id(self):
+
+        xml = self.get_xml(self._URLS[0])
+        url: str = xml.xpath("//li[contains(@id, 'acc-') and position() = 2]/h2[1]/a[1]/@href")[0]
+        id = int(url.split('/')[-1])
+        logger.debug(f'{id=}')
+        return id
 
     @property
     def hashtag(self):
