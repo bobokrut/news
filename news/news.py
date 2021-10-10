dc16d08fc809f940df2deee5082b52e4f966e19d
bobokrut <egor-evlampiev@mail.ru>
2021-10-10 17:22:02 +0300

    add: docstrings for all methonds
    change: deleted hashtag property

--- news/news.py
+++ news/news.py
@@ -1,30 +1,52 @@
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta, timezone
-from time import struct_time, strptime
+from time import strptime, struct_time
 from typing import Union
-import requests
-from lxml import html
 
-from loguru import logger
+import requests
 from config import HEADERS
+from loguru import logger
+from lxml import html
+from lxml.html import HtmlElement
 
 
 class News(ABC):
+    """Base class for news parsing"""
 
-    @property
     @abstractmethod
-    def hashtag(self):
+    def get_new(self) -> dict[str, str]:
+        """
+        Entry method for this class which looks for new news and returns them.
+
+        Returns:
+            dict[str, str]: returns dict {url: text} with all new founded news or is empty if nothing was found
+        """
         pass
 
     @abstractmethod
-    def get_new(self):
+    def _parse(self, xml: HtmlElement, ulr: str) -> dict[str, str]:
+        """
+        Method which should contain parse logic.
+
+        Args:
+            xml (HtmlElement): object from ``get_xml()`` method which calls ``lxml.html.fromstring()`` function
+            ulr (str): url of the website
+
+        Returns:
+            dict[str, str]: returns dict {url: text} with all new founded news or is empty if nothing was found
+        """
         pass
 
-    @abstractmethod
-    def _parse(self, xml, ulr: str):
-        pass
+    def get_xml(self, url: str) -> html.HtmlElement:
+        """
+        Makes request to the website, gets html string and converts it to the parsible object.
 
-    def get_xml(self, url) -> html.HtmlElement:
+        Args:
+            url (str): url of the website to request
+
+        Returns:
+            html.HtmlElement: returts object which can parse given html string
+        """
 
         news = requests.get(url, headers=HEADERS)
         logger.info(f"Page {url} requested with code: {news.status_code}")
@@ -33,18 +55,51 @@ class News(ABC):
         return xml
 
     def filter_out(self, filter: list[str], text: str) -> bool:
+        """
+        Endicates if string conains any word from filter list
+
+        Args:
+            filter (list[str]): list with words to filter
+            text (str): text where to look for
+
+        Returns:
+            bool: True if any word was found in the text else False
+        """
 
         return any(word in text for word in filter)
 
 
 class NewsWithTime(News):
+    """
+    Extended News class which supports parsing articles with time stamp
 
-    def __init__(self, td: Union[int, None]) -> None:
+    Args:
+        News (class): base class
+    """
+
+    def __init__(self, td: int) -> None:
+        """
+        Creates self.time with correct time_struct considering given timezone
+
+        Args:
+            td (int): timezone as `+3` or `-2`
+        """
         super().__init__()
         self.time = (datetime.now(timezone(timedelta(hours=td)))).timetuple() if td else None
         logger.info(f'Started time: {self.time}')
 
     def _check_time_date(self, time_to_check: str, date_time_format: str, current_time: struct_time) -> Union[struct_time, None]:
+        """
+        Checks if given datetime of article is bigger than previous one. If yes returns new `struct_time` else `None`
+
+        Args:
+            time_to_check (str): datetime of article
+            date_time_format (str): format of time_to_check. must have correct syntax according to the default python time formatting
+            current_time (struct_time): of last sent article from certain url. Should be located in ``self.URLS[url]``
+
+        Returns:
+            Union[struct_time, None]: None if all articles from webpage are old else returns time of new article
+        """
 
         date_time_struct: struct_time = strptime(time_to_check, date_time_format)
 
