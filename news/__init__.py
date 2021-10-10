1e6e6d3100115e3e823d015ef1834e77333cde23
bobokrut <egor-evlampiev@mail.ru>
2021-10-10 17:19:37 +0300

    change: added __all__ for better import

---
+++ news/__init__.py
@@ -0,0 +1,6 @@
+from .dtp_ptz import DtpPtz
+from .news import News, NewsWithTime
+from .stolicaonego import StolicaOnego
+from .yle import Yle
+
+__all__ = [DtpPtz, News, NewsWithTime, Yle, StolicaOnego]
