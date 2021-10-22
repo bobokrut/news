1330a159c4b367fdb100a7052e799103bde705fa
bobokrut <egor-evlampiev@mail.ru>
2021-10-22 14:42:40 +0300

    fix: better file matching

--- news/__init__.py
+++ news/__init__.py
@@ -6,11 +6,12 @@ import sys as _sys
 
 from .news import News as _News
 
-_modules = (file[:-3] for file in _os.listdir("news") if re.fullmatch("[a-z]+.py", file))
+_modules = (file[:-3] for file in _os.listdir("news") if re.search("([a-z].py)$", file))
 
 for module in _modules:
     _importlib.import_module(f"news.{module}")
 
 _base_classes = tuple(cls for _, cls in _inspect.getmembers(_sys.modules["news.news"], _inspect.isclass) if cls.__module__ == "news.news")
 sites: tuple[_News, ...] = tuple(site() for cls in _base_classes for site in cls.__subclasses__() if site not in _base_classes)
+print(sites)
 __all__ = ["sites"]
