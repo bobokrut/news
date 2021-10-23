57169b148ffc31de929271074dd51e8e65b5fb3d
bobokrut <egor-evlampiev@mail.ru>
2021-10-23 21:31:42 +0300

    add: started writing tests

---
+++ tests/test_news.py
@@ -0,0 +1,62 @@
+import time
+from logging import INFO
+from unittest.mock import patch
+
+import pytest
+from lxml.html import HtmlElement
+from mylogs import log_setup
+from news import stolicaonego, yle
+from news.news import News
+from requests.exceptions import ConnectionError
+from requests_mock.mocker import Mocker
+
+log_setup()
+
+
+@pytest.fixture
+def news_class():
+    with patch.multiple("news.news.News", __abstractmethods__=set()):
+        return News()
+
+
+@pytest.fixture
+def stolica_onego_class():
+
+    return stolicaonego.StolicaOnego()
+
+
+@pytest.fixture
+def yle_class():
+    return yle.Yle()
+
+
+def test_news_filtering(news_class: News, caplog: pytest.LogCaptureFixture):
+
+    text = ("Коронавирус наступает!", "Начались поиски пропавшей девушки", "Summer vocations are close! Get ready")
+    filters = ("ready", "Корона", "пропавшей")
+
+    for t in text:
+        result = news_class.filter_out(filters, t)
+
+        assert result is True
+
+        with caplog.at_level(INFO):
+            assert caplog.records[-1].message == f"Filtered: {t}"
+
+
+def test_stolica_onego_start_time(stolica_onego_class: stolicaonego.StolicaOnego):
+
+    excpected = time.localtime().tm_hour
+
+    actual = stolica_onego_class.time.tm_hour
+
+    assert excpected == actual
+
+
+def test_yle_start_time(yle_class: yle.Yle):
+
+    excpected = time.localtime().tm_hour
+
+    actual = yle_class.time.tm_hour
+
+    assert excpected == actual
