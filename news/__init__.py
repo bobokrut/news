24f04bc0fb7001352378d115501f5d9d28ac8cf8
bobokrut <egor-evlampiev@mail.ru>
2021-11-15 15:05:15 +0200

    fix: fixed url dict parsing

--- news/__init__.py
+++ news/__init__.py
@@ -11,6 +11,6 @@ with open("urls_conf.json", "r") as _f:
 
 _base_classes = tuple(cls for _, cls in _inspect.getmembers(_sys.modules["news.news"], _inspect.isclass) if cls.__module__ == "news.news")
 
-sites: tuple[_News, ...] = tuple(site(tuple(_j_urls[site.__name__])) for cls in _base_classes for site in cls.__subclasses__() if site not in _base_classes)
+sites: tuple[_News, ...] = tuple(site(tuple(_j_urls[site.__name__]["urls"])) for cls in _base_classes for site in cls.__subclasses__() if site not in _base_classes)
 
 del _j_urls
