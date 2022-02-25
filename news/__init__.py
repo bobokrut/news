b05461d806e76e434cace0b08f3c3395a12a9925
bobokrut <egor-evlampiev@mail.ru>
2022-02-25 17:33:05 +0200

    Added option to disable/enable site parsing

--- news/__init__.py
+++ news/__init__.py
@@ -11,6 +11,7 @@ with open("urls_conf.json", "r") as _f:
 
 _base_classes = tuple(cls for _, cls in _inspect.getmembers(_sys.modules["news.news"], _inspect.isclass) if cls.__module__ == "news.news")
 
-sites: tuple[_News, ...] = tuple(site(tuple(_j_urls[site.__name__]["urls"])) for cls in _base_classes for site in cls.__subclasses__() if site not in _base_classes)
+sites: tuple[_News, ...] = tuple(site(tuple(_j_urls[site.__name__]["urls"])) for cls in _base_classes for site in cls.__subclasses__() if site not in _base_classes and _j_urls[site.__name__]["active"])
+print(len(sites))
 
 del _j_urls
