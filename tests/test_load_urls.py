3726555f3273212f5b357b0b7613f6bfc9c267cb
bobokrut <egor-evlampiev@mail.ru>
2022-04-07 17:04:21 +0300

    change: changed tests

--- tests/test_load_urls.py
+++ tests/test_load_urls.py
@@ -1,12 +1,16 @@
 import main
+from time import struct_time
 
 def test_url_load_not_empty():
 
     result = main.load_urls()
     assert result != {}
 
-def test_yle_load_urls_only_active():
+def test_yle_load_check_attrs_and_types():
 
-    result = main.load_urls()
-    assert bool(s["active"] for s in result.values()) == True
-    assert bool(s.get("time") for s in result.values()) == True
+    result = main.load_urls()[0]
+    assert isinstance(result.sitename, str)
+    assert isinstance(result.url, str)
+    assert isinstance(result.url_desc, str)
+    assert isinstance(result.time, struct_time)
+
