5f77b14b11b5f475aca0145820acd506f73c73a7
bobokrut <egor-evlampiev@mail.ru>
2022-04-10 13:17:11 +0300

    fix: fixed handling of empty articles in meduza
    change: updated package version

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def parse_text(text: str, url_desc: str) -> str:  # type: ignore
         case text, _ if len(text) < 3 or not text:
             return ""
         case _, "meduza":
-            return text.split("<p>")[3]
+            return t if len(t := text.split("<p>")[3]) > 3 else ""
         case _:
             return text
 
